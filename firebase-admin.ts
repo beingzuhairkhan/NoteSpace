4be42a3185a92d487de16da9035641738cec66fa
zuhair khan <zuhairkhan5134@gmail.com>
2024-12-14 23:11:29 +0530

    update admin

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,20 +1,15 @@
-import { 
-    initializeApp, 
-    getApps, 
-    App, 
-    getApp, 
-    cert 
-} from 'firebase-admin/app';
+import { initializeApp, getApps, App, getApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
-import serviceKey from './service_Key.json'; // Adjust path as needed
+import * as admin from 'firebase-admin'; // Import the 'admin' namespace for types
+
+import * as serviceAccount from './service_Key.json';
 
 let app: App;
-
 // Check if Firebase Admin app is initialized
 if (getApps().length === 0) {
-    // Initialize Firebase Admin if not already initialized
+    // Initialize Firebase Admin with cert object
     app = initializeApp({
-        credential: cert(serviceKey),
+        credential: cert(serviceAccount as admin.ServiceAccount), // Use 'admin.ServiceAccount' type here
     });
 } else {
     // Use existing app instance if already initialized
