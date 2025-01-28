5d39756345a0f11b3782c5ef03711daf5940a528
zuhair khan <zuhairkhan5134@gmail.com>
2025-01-29 01:47:01 +0530

    .env

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -2,31 +2,39 @@ import {
     initializeApp, 
     getApps, 
     App, 
-    getApp, 
-    cert 
+    getApp 
 } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import * as admin from 'firebase-admin';
-// import path from 'path'
 
-//const serviceAccount = require(path.resolve(__dirname, './admin.json'));
- const serviceAccount = require('./admin.json');
-// Declare the Firebase app instance
+
+
+
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
+
 let app: App;
 
-
 if (getApps().length === 0) {
-
     app = initializeApp({
-        credential: admin.credential.cert(serviceAccount),
+        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
     });
     console.log("Firebase admin app initialized successfully.");
 } else {
-    // Use the already initialized app
     app = getApp();
     console.log("Firebase app already initialized, using existing instance.");
 }
-
 
 const adminDB = getFirestore(app);
 
